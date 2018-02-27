44c1d492b86bfdef5cfac4dddb60717de1eec1da
Robin <Robin@DESKTOP-1C44T3I.lan>
2018-02-27 09:05:25 +0100

    Spelled my name wrong. Added imports to event.

--- Lab5/src/simulator/State.java
+++ Lab5/src/simulator/State.java
@@ -1,10 +1,11 @@
 package simulator;
 
 import java.util.*;
+import simulator.modifiers.Event;
 
 /**
  * 
- * @author Chonratid Pangdee, Anton Johansson, Ronbin Danielsson & Zerophymyr Falk
+ * @author Chonratid Pangdee, Anton Johansson, Robin Danielsson, Zerophymyr Falk
  *
  */
 public class State extends Observable {
