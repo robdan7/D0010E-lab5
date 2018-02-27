99a0e450cb91b35760eb3ce004dd331532985f15
Robin <Robin@DESKTOP-1C44T3I.lan>
2018-02-27 12:51:34 +0100

    Added sortedItem implementation. Read the javadoc for implementation details.

--- Lab5/src/simulator/modifiers/Event.java
+++ Lab5/src/simulator/modifiers/Event.java
@@ -1,14 +1,21 @@
 package simulator.modifiers;
 
+import simulator.queue.SortedItem;
+
 /**
  * 
  * @author Chonratid Pangdee, Anton Johansson, Robin Danielsson, Zerophymyr Falk
  *
  */
-public class Event {
-	
+public class Event implements SortedItem{
+
 	public float getTime() {
 		return 0;
+	}
+
+	@Override
+	public boolean largerThan(SortedItem item) {
+		return true;
 	}
 
 }
