4fa3ae8c0e2e02acde1ef378fe7351824f0072cf
Robin <Robin@LAPTOP-AEI59GSM>
2018-03-01 11:43:45 +0100

    Added empty javadoc.

--- Lab5/src/market/RunSim.java
+++ Lab5/src/market/RunSim.java
@@ -7,11 +7,23 @@ import simulator.queue.EventQueue;
 
 public class RunSim {
 
+	/**
+	 * 
+	 * @param maxCustomers
+	 * @param maxCheckouts
+	 * @param arrivalTime
+	 * @param checkoutTime
+	 * @param pickingTime
+	 * @param closingTime
+	 * @param endTime
+	 * @param seed
+	 */
 	public RunSim(int maxCustomers, int maxCheckouts, double arrivalTime, double[] checkoutTime, double[] pickingTime,
 			double closingTime, double endTime, long seed) {
 		
 		
 		MarketState s = new MarketState(maxCustomers, arrivalTime, checkoutTime, pickingTime, maxCheckouts, seed);
+		MarketView view = new MarketView(s);
 		EventQueue q = new EventQueue();
 		Event open = new OpeningEvent(q, closingTime, endTime);
 		Simulator sim = new Simulator(s, q, open, 0, 0, 0);
